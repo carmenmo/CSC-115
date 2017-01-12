cf97c1416f59488b3716256b20629691e775e402
Carmen Overton <carmeno@uvic.ca>
2017-01-11 18:51:25 -0800

    prismTest.java
    Outputs the properties of two rectangles

---
+++ prismTest.java
@@ -0,0 +1,13 @@
+public class prismTest{
+
+	public static void main (String[] args){
+	
+		Prism p1 = new Prism(2, 3, 4, 5);
+		Prism p2 = new Prism(8, 6, 4, 2);
+		
+		System.out.println("Prism 1: \t" + p1);
+		System.out.println("Prism 2: \t" + p2);
+	
+	}
+
+}
